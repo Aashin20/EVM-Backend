2767d26b5aadbaf4a4a5cc047cc2a6a5d1a1c076
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-05-22 12:34:47 +0530

    Initialize Fastapi

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from core.db import Database
+import uvicorn
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+from fastapi import Depends
+
+app= FastAPI()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
