37ba9bab4a5f759db8a8a7a15a05b4f9d4974ba8
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-05-22 12:35:06 +0530

    Create middlewares

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ from fastapi import Depends
 
 app= FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
