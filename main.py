a9d5492c3d11c458ada8754a9d57ccea97bdf0a5
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-05-22 12:37:29 +0530

    Create health check endpoint

--- main.py
+++ main.py
@@ -30,5 +30,10 @@ async def lifespan(app: FastAPI):
     Database._engine.dispose()
     print("Disconnected from Database")
 
+app=FastAPI(lifespan=lifespan)
+@app.get("/health")
+async def health_check():
+    return {"status": "ok"}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
