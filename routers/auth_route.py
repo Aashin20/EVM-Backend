991aa4f30a63e71c91ef27ac3e2d3cbabcecf9a3
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-05-26 16:51:32 +0530

    Implement login route

--- routers/auth_route.py
+++ routers/auth_route.py
@@ -8,3 +8,6 @@ async def register_user(username: str, password: str, role: str):
     return register(username, password, role)
 
 
+@router.post("/login")
+async def login_user(username: str, password: str):
+    return login(username, password)
