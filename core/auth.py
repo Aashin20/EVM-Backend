2e593e9eabf4c1c085b472ae8a8fb83a018302c1
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-05-28 16:18:49 +0530

    Update register logic

--- core/auth.py
+++ core/auth.py
@@ -40,3 +40,44 @@ def login(username, password):
 
 
 
+def register(details: RegisterModel):
+    with Database.get_session() as session:
+        existing_user = session.query(User).filter(User.username == details.username).first()
+        if existing_user:
+            return {"error": "Username already exists"}
+        
+        hashed_password = bcrypt.hashpw(details.password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        
+        local_body_id = details.local_body_id if details.local_body_id not in [0, "0"] else None
+        warehouse_id = details.warehouse_id if details.warehouse_id not in [0, "0"] else None
+        new_user = User(
+            username=details.username,
+            password_hash=hashed_password,
+            role_id=details.role_id,
+            level_id=details.level_id,
+            district_id=details.district_id,
+            local_body_id=local_body_id,
+            warehouse_id=warehouse_id
+        )
+        session.add(new_user)
+        session.commit()
+        session.refresh(new_user)
+        role_name = new_user.role.name
+        level_name = new_user.level.name
+        user_id = new_user.id
+        username = new_user.username
+
+    token = create_token({
+        "username": username,
+        "role": role_name,
+        "level": level_name,
+        "user_id": user_id
+    })
+
+    return {
+        "token": token,
+        "role": role_name,
+        "level": level_name,
+        "user_id": user_id
+    }
+
