5521bbfd3f6d99e8ede9f2d1a9140ab7870036c4
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-05-28 16:18:37 +0530

    Update login logic

--- core/auth.py
+++ core/auth.py
@@ -2,15 +2,31 @@ from utils.authtoken import create_token, verify_token
 from .db import Database
 from models.users import User
 import bcrypt
+from pydantic import BaseModel, constr
+from typing import Optional
+from sqlalchemy.orm import joinedload
 
+class RegisterModel(BaseModel):
+    username: constr(strip_whitespace=True, min_length=3)
+    password: constr(min_length=6)
+    role_id: int
+    level_id: int
+    district_id: int
+    local_body_id: Optional[int] = None
+    warehouse_id: Optional[int] = None
 
 
 def login(username, password):
 
     with Database.get_session() as session:
-        current = session.query(User).filter(User.username == username).first()
+        current = session.query(User).options(
+            joinedload(User.role),
+            joinedload(User.level)
+        ).filter(User.username == username).first()
         if not current:
             return {"error": "User not found"}
+        if current.is_active is False:
+            return {"error": "Account has been deactivated, Please contact support"}
 
     password_hash = current.password_hash
     verification = bcrypt.checkpw(password.encode('utf-8'), password_hash.encode('utf-8'))
@@ -18,23 +34,9 @@ def login(username, password):
     if not verification:
         return {"error": "Invalid password"}
 
-    token = create_token({"username": current.username, "role": current.role})
+    token = create_token({"username": current.username, "role": current.role.name,"level": current.level.name, "user_id": current.id})
 
     return {"token": token, "role": current.role, "username": current.username}
 
 
 
-def register(username, password, role):
-    with Database.get_session() as session:
-        existing_user = session.query(User).filter(User.username == username).first()
-        if existing_user:
-            return {"error": "Username already exists"}
-
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-        new_user = User(username=username, password_hash=hashed_password, role=role)
-        session.add(new_user)
-        session.commit()
-        session.refresh(new_user)
-
-    token = create_token({"username": new_user.username, "role": new_user.role})
-    return {"token": token, "role": new_user.role, "username": new_user.username}
