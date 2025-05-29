2f39d10808e62a41545d625e633fdec5c6b6de28
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-05-29 13:20:36 +0530

    Create component model

--- models/evm.py
+++ models/evm.py
@@ -3,6 +3,15 @@ from sqlalchemy import (
     ForeignKey, Enum
 )
 from sqlalchemy.orm import relationship
-from datetime import datetime
+from datetime import datetime, timezone
 import enum
 from core.db import Base
+from zoneinfo import ZoneInfo
+
+class EVMComponentType(str, enum.Enum):
+    CU = "CU"
+    BU = "BU"
+    DMM = "DMM"
+    DMM_SEAL = "DMM_SEAL"
+    PINK_PAPER_SEAL = "PINK_PAPER_SEAL"
+
