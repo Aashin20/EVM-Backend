bbefb484f1cee237d8be5cc44a322a73e8854d30
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-02 11:36:25 +0530

    Update allotment route
    Create route to approve allotment

--- routers/allot_route.py
+++ routers/allot_route.py
@@ -7,3 +7,7 @@ router = APIRouter()
 async def allot_evm(data: AllotmentModel):
     return create_allotment(data)
 
+@router.post("/approve/{allotment_id}")
+async def approve(allotment_id: int,current_user_id: int=1):  #Replace using depends
+    return approve_allotment(allotment_id,current_user_id)
+    
