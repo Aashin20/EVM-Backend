861c38795ca1b97d37448f7505688140b8625128
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-02 11:34:29 +0530

    Update allotments.py
    Create allotment logic

--- core/allotment.py
+++ core/allotment.py
@@ -31,3 +31,50 @@ class AllotmentResponse(BaseModel):
 
     class Config:
         orm_mode = True
+
+def create_allotment(data: AllotmentModel):
+    with Database.get_session() as session:
+        components = session.query(EVMComponent).filter(EVMComponent.id.in_(data.evm_component_ids)).all()
+        if len(components) != len(data.evm_component_ids):
+            raise HTTPException(status_code=404, detail="One or more EVM components not found.")
+        
+        for comp in components:
+            if comp.status in ["used", "returned", "flc_failed", "faulty"]:
+                raise HTTPException(400, f"Component {comp.serial_number} is not available for allotment.")
+            if comp.current_user_id != data.from_user_id:
+                raise HTTPException(status_code=403, detail=f"Component {comp.serial_number} is not owned by the sender.")
+
+        allotment = Allotment(
+            allotment_type=data.allotment_type.value,
+            from_user_id=data.from_user_id,
+            to_user_id=data.to_user_id,
+            from_local_body_id=data.from_local_body_id,
+            to_local_body_id=data.to_local_body_id,
+            from_district_id=data.from_district_id,
+            to_district_id=data.to_district_id,
+            initiated_by_id=data.from_user_id,  #Fix for prod
+            is_return=(data.allotment_type.value == "Return"),
+            return_reason=data.return_reason,
+            original_allotment_id=data.original_allotment_id
+        )
+        session.add(allotment)
+        session.commit()
+        session.refresh(allotment)
+
+        for component in components:
+            item = AllotmentItem(
+                allotment_id=allotment.id,
+                evm_component_id=component.id
+            )
+            session.add(item)
+
+        session.commit()
+        return {
+            "id": allotment.id,
+            "allotment_type": allotment.allotment_type.value,
+            "from_user_id": allotment.from_user_id,
+            "to_user_id": allotment.to_user_id,
+            "status": allotment.status,
+            "evm_component_ids": data.evm_component_ids
+        }
+
