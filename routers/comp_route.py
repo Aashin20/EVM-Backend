0f957510c34170b507ceeb3e4eacfcb95098003f
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-02 11:36:50 +0530

    Update components route
    Create endpoint to get cu numbers from a district

--- routers/comp_route.py
+++ routers/comp_route.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
-from core.components import new_component, ComponentModel
+from core.components import new_component, ComponentModel,view_cu
 
 router = APIRouter()
 
 @router.post("/new")
 async def add_component(details: ComponentModel):
     return new_component(details)
+
+@router.get("/cu/{district_id}")
+async def cu(district_id: int):
+    return view_cu(district_id)
