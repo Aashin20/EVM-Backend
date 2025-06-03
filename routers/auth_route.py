998cf5ab3601c3b5787e17fff9118a3ea0e91e7c
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-04 01:42:09 +0530

    Update auth_route

--- routers/auth_route.py
+++ routers/auth_route.py
@@ -1,13 +1,8 @@
 from fastapi import APIRouter
-from core.auth import register, login
-from core.auth import RegisterModel
+from core.user import register, login
+from core.user import RegisterModel
 
 router = APIRouter()
-
-@router.post("/register")
-async def register_user(details: RegisterModel):
-    return register(details)
-
 
 @router.post("/login")
 async def login_user(username: str, password: str):
