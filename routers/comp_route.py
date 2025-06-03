bb83d050ce9adc7a4e949b76ca991488c65a4a9e
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-03 13:07:56 +0530

    Update component route
    Taking input as a list

--- routers/comp_route.py
+++ routers/comp_route.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter
-from core.components import new_component, ComponentModel,view_cu
+from core.components import new_components, ComponentModel,view_cu
+from typing import List
 
 router = APIRouter()
 
 @router.post("/new")
-async def add_component(details: ComponentModel):
-    return new_component(details)
+def create_new_components(components: List[ComponentModel]):
+    return new_components(components)
 
-@router.get("/cu/{district_id}")
-async def cu(district_id: int):
-    return view_cu(district_id)
+@router.get("/cu/{user_id}")
+async def cu(user_id: int):
+    return view_cu(user_id)
