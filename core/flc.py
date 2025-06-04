72823840b7d653e7b9d0dcbb18e04e14934dd9be
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-05 02:14:02 +0530

    Update flc.py
    Fn to conduct flc for bu

--- core/flc.py
+++ core/flc.py
@@ -25,3 +25,28 @@ class FLCBUModel(BaseModel):
 
 
 
+
+
+
+
+def flc_bu(datas: list[FLCBUModel]):
+    with Database.get_session() as session:
+        for data in datas:
+            bu = session.query(EVMComponent).filter_by(serial_number=data.bu_serial).first()
+            if not all([bu]):
+                raise HTTPException(status_code=404, detail=data.bu_serial)
+            
+            flc = FLCBallotUnit(
+                bu_id=bu.id,
+                box_no=data.box_no,
+                passed=data.passed,
+                remarks=data.remarks,
+                flc_by_id=data.flc_by_id
+            )
+            session.add(flc)
+            if data.passed:
+                bu.status = "FLC_Passed"
+            else:
+                bu.status = "FLC_Failed"
+        session.commit()
+    return 200
