9f0746627415a47190e467b777ad225be36c53bf
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-05 02:16:58 +0530

    Update flc_route.py
    Endpoint to conduct FLC for CU

--- routers/flc_route.py
+++ routers/flc_route.py
@@ -2,3 +2,8 @@ from fastapi import APIRouter
 from core.flc import flc_cu, FLCCUModel, FLCBUModel, flc_bu
 from typing import List
 
+router = APIRouter()
+
+@router.post("/cu")
+async def flc_cu_bulk(data: List[FLCCUModel]):
+    return flc_cu(data)
