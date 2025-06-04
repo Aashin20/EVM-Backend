6aa297157e99aee642dc10fb22feee11f8afa655
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-05 02:13:22 +0530

    Update flc.py
    Create pydantic models

--- core/flc.py
+++ core/flc.py
@@ -6,3 +6,22 @@ from typing import Optional, List
 from fastapi import Response
 
 
+class FLCCUModel(BaseModel):
+    cu_serial: str
+    dmm_serial: str
+    dmm_seal_serial: str
+    pink_paper_seal_serial: str
+    box_no: str
+    passed: bool
+    remarks: Optional[str] = None
+    flc_by_id: int
+
+class FLCBUModel(BaseModel):
+    bu_serial: str
+    box_no: str
+    passed: bool
+    remarks: Optional[str] = None
+    flc_by_id: int
+
+
+
