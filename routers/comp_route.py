23d42fd5c4eafcb730663629e7713afe50827ec2
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-04 12:18:15 +0530

    Update comp_route.py
    Change route name

--- routers/comp_route.py
+++ routers/comp_route.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from core.components import new_components, ComponentModel,view_components
+from core.components import new_components, ComponentModel,view_paired_cu,view_components
 from typing import List
 
 router = APIRouter()
@@ -8,6 +8,7 @@ router = APIRouter()
 def create_new_components(components: List[ComponentModel]):
     return new_components(components)
 
-@router.get("/view/")
+@router.get("/view/{user_id}/{component_type}")
 async def cu(component_type:str,user_id: int):
-    return view_components(component_type,user_id)
+    return view_components(component_type.upper(),user_id)
+
