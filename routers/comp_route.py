b68d87681f22fa424f9d21be827557a578015b49
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-05 02:15:37 +0530

    Update comp_route.py
    Minor fixes

--- routers/comp_route.py
+++ routers/comp_route.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter
-from core.components import new_components, ComponentModel,view_paired_cu,view_components
+from core.components import new_components, ComponentModel,view_paired_cu,view_components, view_paired_bu
 from typing import List
+from pydantic import BaseModel
+
+class PairedCU(BaseModel):
+    user_id : int
+
+class PairedBU(BaseModel):
+    user_id : int
 
 router = APIRouter()
 
@@ -8,10 +15,11 @@ router = APIRouter()
 def create_new_components(components: List[ComponentModel]):
     return new_components(components)
 
-@router.get("/view/{user_id}/{component_type}")
+@router.get("/view/unpaired/{user_id}/{component_type}")
 async def cu(component_type:str,user_id: int):
     return view_components(component_type.upper(),user_id)
 
-@router.get("/paired/{user_id}")
+@router.get("/view/paired/cu/{user_id}")
 async def paired_cu(user_id: int):
     return view_paired_cu(user_id)
+
