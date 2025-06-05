1f2bac625f6b68dee4223fcdf23b97a63e1e4a5a
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-05 13:27:20 +0530

    Update allot_route.py
    Add JWT Role based access

--- routers/allot_route.py
+++ routers/allot_route.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from core.allotment import create_allotment, AllotmentModel, AllotmentResponse,approve_allotment
+from utils.authtoken import get_current_user
 
 router = APIRouter()
 
 @router.post("/", response_model=AllotmentResponse)
-async def allot_evm(data: AllotmentModel):
+async def allot_evm(data: AllotmentModel,current_user: dict = Depends(get_current_user)):
     return create_allotment(data)
 
 @router.post("/approve/{allotment_id}")
-async def approve(allotment_id: int,current_user_id: int=1):  #Replace using depends
-    return approve_allotment(allotment_id,current_user_id)
+async def approve(allotment_id: int,current_user: dict = Depends(get_current_user)):  
+    return approve_allotment(allotment_id,current_user['id'])
     
