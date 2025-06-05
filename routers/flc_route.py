0e0be91d56adc3861d1fdc0b71b76998c90c75c5
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-05 13:27:44 +0530

    Update flc_route.py
    Add JWT Role based access

--- routers/flc_route.py
+++ routers/flc_route.py
@@ -1,13 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from core.flc import flc_cu, FLCCUModel, FLCBUModel, flc_bu
 from typing import List
+from utils.authtoken import get_current_user
 
 router = APIRouter()
 
 @router.post("/cu")
-async def flc_cu_bulk(data: List[FLCCUModel]):
-    return flc_cu(data)
+async def flc_cu_bulk(data: List[FLCCUModel], current_user: dict = Depends(get_current_user)):
+    if current_user['role'] not in ['Developer', 'FLC Officer']:
+        return {"status": 401, "message": "Unauthorized access"}
+    else:
+        return flc_cu(data)
 
 @router.post("/bu")
-async def flc_bu_bulk(data: List[FLCBUModel]):
-    return flc_bu(data)
+async def flc_bu_bulk(data: List[FLCBUModel],current_user: dict = Depends(get_current_user)):
+    if current_user['role'] not in ['Developer', 'FLC Officer']:
+        return {"status": 401, "message": "Unauthorized access"}
+    else:
+        return flc_bu(data)
