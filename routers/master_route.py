35d05f63deac8e45b5f732d6435750fd502ff1f6
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-05 13:27:52 +0530

    Update master_route.py
    Add JWT Role based access

--- routers/master_route.py
+++ routers/master_route.py
@@ -1,16 +1,27 @@
 from fastapi import APIRouter   
 from core.user import register, RegisterModel, view_users,UpdateUserModel,edit_user
+from utils.authtoken import get_current_user
+from fastapi import Depends
 
 router = APIRouter()
 
 @router.post("/user/create")
-async def register_user(details: RegisterModel):
-    return register(details)
+async def register_user(details: RegisterModel, current_user: dict = Depends(get_current_user)):
+    if current_user['role'] not in ['Developer', 'SEC']:
+        return {"status" : 401, "message": "Unauthorized access"}
+    else:
+        return register(details)
 
 @router.get("/user/view") #SEC ONLY
-async def view():
-    return view_users()
+async def view(current_user: dict = Depends(get_current_user)):
+    if current_user['role'] not in ['Developer', 'SEC']:
+        return {"status": 401, "message": "Unauthorized access"}
+    else:
+        return view_users()
 
 @router.post("/user/edit")
-async def edit(details: UpdateUserModel):
-    return edit_user(details)
+async def edit(details: UpdateUserModel,current_user: dict = Depends(get_current_user)):
+    if current_user['role'] not in ['Developer', 'SEC']:
+        return {"status": 401, "message": "Unauthorized access"}
+    else:
+        return edit_user(details)
