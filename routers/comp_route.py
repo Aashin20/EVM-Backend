0028e126535080b9ed682f83518e65cb61bc579d
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-05 13:27:34 +0530

    Update comp_route.py
    Add JWT Role based access

--- routers/comp_route.py
+++ routers/comp_route.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends,HTTPException
 from core.components import new_components, ComponentModel,view_paired_cu,view_components, view_paired_bu
 from typing import List
 from pydantic import BaseModel
+from utils.authtoken import get_current_user
+
 
 class PairedCU(BaseModel):
     user_id : int
@@ -12,17 +14,20 @@ class PairedBU(BaseModel):
 router = APIRouter()
 
 @router.post("/new")
-def create_new_components(components: List[ComponentModel]):
-    return new_components(components)
+def create_new_components(components: List[ComponentModel], current_user: dict = Depends(get_current_user)):
+    if current_user['role'] not in ['Developer', 'SEC','DEO', 'FLC Officer']:
+        raise HTTPException(status_code=401, detail="Unauthorized access")
+    else:
+        return new_components(components)
 
 @router.get("/view/unpaired/{user_id}/{component_type}")
-async def cu(component_type:str,user_id: int):
+async def cu(component_type:str,user_id: int,current_user: dict = Depends(get_current_user)):
     return view_components(component_type.upper(),user_id)
 
 @router.get("/view/paired/cu/{user_id}")
-async def paired_cu(user_id: int):
+async def paired_cu(user_id: int,current_user: dict = Depends(get_current_user)):
     return view_paired_cu(user_id)
 
 @router.get("/view/paired/bu/{user_id}")
-async def paired_bu(user_id: int):
+async def paired_bu(user_id: int,current_user: dict = Depends(get_current_user)):
     return view_paired_bu(user_id)
