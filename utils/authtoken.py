316d174d35352655f2a788fd9416ef443ddb3021
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-05 13:29:31 +0530

    Update auth_token.py
    Create fn to get current user

--- utils/authtoken.py
+++ utils/authtoken.py
@@ -2,13 +2,16 @@ from jose import jwt, ExpiredSignatureError, JWTError
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
-from fastapi import HTTPException
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login") 
 
 def create_token(details: dict, expiry: int = 30):
     to_encode = details.copy()
@@ -28,3 +31,13 @@ def verify_token(token: str):
         raise HTTPException(status_code=401, detail="Invalid token")
     except Exception as e:
         return {"error": str(e)}
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    payload = verify_token(token)
+    if not payload:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return payload
