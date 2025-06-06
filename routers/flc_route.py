799c44a5c3f928efed51f6031a2a7abbb9af6425
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-06 11:38:13 +0530

    Update flc_route.py
    Minor Fixes

--- routers/flc_route.py
+++ routers/flc_route.py
@@ -2,15 +2,17 @@ from fastapi import APIRouter, Depends
 from core.flc import flc_cu, FLCCUModel, FLCBUModel, flc_bu
 from typing import List
 from utils.authtoken import get_current_user
+from fastapi import HTTPException
 
 router = APIRouter()
 
 @router.post("/cu")
 async def flc_cu_bulk(data: List[FLCCUModel], current_user: dict = Depends(get_current_user)):
+    print(f"Current user role: {current_user['role']}", flush=True)
     if current_user['role'] not in ['Developer', 'FLC Officer']:
-        return {"status": 401, "message": "Unauthorized access"}
+        raise HTTPException(status_code=401, detail="Unauthorized access")
     else:
-        return flc_cu(data)
+        return flc_cu(data,current_user['user_id'])
 
 @router.post("/bu")
 async def flc_bu_bulk(data: List[FLCBUModel],current_user: dict = Depends(get_current_user)):
