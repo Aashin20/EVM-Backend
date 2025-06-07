0f4c31fcf663f17ddbcb95083fa61cec724d233a
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-07 12:40:41 +0530

    Update allot_route.py
    Minor Fixes

--- routers/allot_route.py
+++ routers/allot_route.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
-from core.allotment import create_allotment, AllotmentModel, reject_allotment,approve_allotment, approval_queue
+from core.allotment import create_allotment, AllotmentModel, reject_allotment,approve_allotment, approval_queue, evm_commissioning,EVMCommissioningModel
 from utils.authtoken import get_current_user
 from typing import List
 
@@ -7,7 +7,7 @@ router = APIRouter()
 
 @router.post("/")
 async def allot_evm(data: AllotmentModel,current_user: dict = Depends(get_current_user)):
-    return create_allotment(data)
+    return create_allotment(data,current_user['user_id'])
 
 @router.get("/approve/{allotment_id}")
 async def approve(allotment_id: int,current_user: dict = Depends(get_current_user)):  
@@ -20,4 +20,4 @@ async def reject(allotment_id: int,current_user: dict = Depends(get_current_user
 @router.get("/queue/")
 async def queue(current_user: dict = Depends(get_current_user)):
     return approval_queue(current_user['user_id'])
-    
+
