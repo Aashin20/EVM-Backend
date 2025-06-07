c22f83356117968337c88231f9db9e3dc100c2bb
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-07 12:41:22 +0530

    Update meta_route.py
    Minor Fixes

--- routers/meta_route.py
+++ routers/meta_route.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends
-from core.user import get_local_body
+from core.user import get_local_body,get_districts,get_panchayath,get_user,get_RO
 from utils.authtoken import get_current_user
 
 router = APIRouter()
 
 @router.get("/district/{district_id}/{type}")
-async def local_body(district_id: int,type: str, current_user: dict = Depends(get_current_user)):
+async def local_body(district_id:int,type: str,current_user: dict = Depends(get_current_user)):
     return get_local_body(district_id,type)
+
