0b8cb6f0d1bf3ea78215a8a8be6bcc029b243352
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-08 00:37:19 +0530

    Minor Fix

--- routers/return_route.py
+++ routers/return_route.py
@@ -1,9 +1,11 @@
-from core.return_ import status_change
-from fastapi import APIRouter
+from core.return_ import status_change,decommission_evms
+from utils.authtoken import get_current_user
+from fastapi import APIRouter, Depends
+from typing import List
 
 router = APIRouter()
 
 
 @router.get('/{local_body_id}/{status}')
-async def to_polling(local_body_id:str,status:str):
+async def to_polling(local_body_id:str,status:str,current_user: dict = Depends(get_current_user)):
     return status_change(local_body_id,status)
