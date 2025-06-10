2cb8d26c77ece3620d0c1692b6442cb85814e01b
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-10 13:57:54 +0530

    Create endpoint for evm decomissioning

--- routers/return_route.py
+++ routers/return_route.py
@@ -1,4 +1,4 @@
-from core.return_ import status_change,decommission_evms
+from core.return_ import status_change,decommission_evms,DecommissionModel
 from utils.authtoken import get_current_user
 from fastapi import APIRouter, Depends
 from typing import List
@@ -11,5 +11,5 @@ async def to_polling(local_body_id:str,status:str,current_user: dict = Depends(g
     return status_change(local_body_id,status)
 
 @router.post('/decommission')
-async def evm_decommission(local_body_id: str, evm_ids: List[str],current_user: dict = Depends(get_current_user)):
-    return decommission_evms(local_body_id, evm_ids)
+async def evm_decommission(data: DecommissionModel):
+    return decommission_evms(data)
