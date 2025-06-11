eb4588ca19982ae0a8758b968e0319b85cac3e3b
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-11 13:14:07 +0530

    Create endpoint to fetch allotment logs

---
+++ routers/logs_route.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, Query,Depends
+from utils.authtoken import get_current_user
+from typing import Optional
+from datetime import date
+from core.logs import (
+    get_allotment_logs_data,
+    get_allotment_item_logs_data,
+    get_component_logs_data,
+    get_pairing_logs_data,
+    get_flc_record_logs_data,
+    get_flc_bu_logs_data,
+    get_all_logs_data
+)
+from fastapi.exceptions import HTTPException
+
+
+router = APIRouter()
+
+
+@router.get("/allotments")
+def get_allotment_logs(
+    page: int = Query(1, ge=1),
+    page_size: int = Query(50, ge=1, le=100),
+    start_date: Optional[date] = None,
+    end_date: Optional[date] = None,
+    current_user: dict = Depends(get_current_user)
+):
+    """Get allotment logs with pagination and date filtering"""
+    if current_user['role'] not in ['SEC']:
+        raise HTTPException(status_code=401, detail="Unauthorized access")
+    return get_allotment_logs_data(page, page_size, start_date, end_date)
+
+
