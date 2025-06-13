db278f2fd2ddc8f4fbc3de8fde0b9c5cc2e4c25a
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-13 10:26:53 +0530

    Update comp routes

--- routers/comp_route.py
+++ routers/comp_route.py
@@ -23,38 +23,44 @@ async def create_new_components(components: List[ComponentModel],order_no:str,cu
     else:
         return new_components(components, order_no,current_user['user_id'])
 
-@router.get("/view/unpaired/{component_type}/{district_id}")
+@router.get("/msr/unpaired/{component_type}/{district_id}")
 async def cu(component_type:str,district_id:str =Path(...),current_user: dict = Depends(get_current_user)):
     try:
         district_id=int(district_id)
         if current_user['role']=='DEO':
             return view_components_deo(component_type,district_id)
-        else:
-            return view_components(component_type.upper(),current_user['user_id'])
     except (ValueError, TypeError):
         return view_components_sec(component_type)
     
-    
+@router.get('/view/unpaired/{component_type}')
+async def view_unpaired(component_type:str,current_user:dict = Depends(get_current_user)):
+    return view_components(component_type.upper(),current_user['user_id'])
 
-@router.get("/view/paired/cu/{district_id}")
-async def paired_cu(district_id:int,current_user: dict = Depends(get_current_user)):
-    if current_user['role']=='SEC':
+@router.get("/msr/paired/cu/{district_id}")
+async def paired_cu(district_id:str=Path(...),current_user: dict = Depends(get_current_user)):
+    try:
+        district_id = int(district_id)
+        if current_user['role'] == 'DEO':
+            return view_paired_cu_deo(district_id)
+    except (ValueError, TypeError):
         return view_paired_cu_sec()
-    elif current_user['role'] == 'DEO':
-        return view_paired_cu_deo(district_id)
-    else:
-        return view_paired_cu(current_user['user_id'])
+    
+@router.get("/view/paired/cu")
+async def get_paired_cu(current_user: dict = Depends(get_current_user)):
+    return view_paired_cu(current_user['user_id'])
 
-@router.get("/view/paired/bu/{district_id}")
+@router.get("/msr/paired/bu/{district_id}")
 async def paired_bu(district_id: str = Path(...),current_user: dict = Depends(get_current_user)):
     try:
         district_id = int(district_id)
 
         if current_user['role'] == 'DEO':
             return view_paired_bu_deo(district_id)
-        else:
-            return view_paired_bu(current_user['user_id'])
 
     except (ValueError, TypeError):
         return view_paired_bu_sec()
+    
+@router.get("/view/paired/bu")
+async def get_paired_bu(current_user: dict = Depends(get_current_user)):
+    return view_paired_bu(current_user['user_id'])
       
