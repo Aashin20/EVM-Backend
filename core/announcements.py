9c141b47615aa447ebf1c7565313a86eb8aef17d
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-26 09:01:11 +0530

    Create fn to view announcements

--- core/announcements.py
+++ core/announcements.py
@@ -12,3 +12,20 @@ def create_announcement(content: str,from_user_id: int,to_user:str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error creating announcement: {str(e)}")
     
+def view_announcements(user_id: int,role: str):
+    try:
+        user_id = str(user_id)
+        with Database().get_session() as db:
+            announcements=db.query(Announcements).filter(
+                (Announcements.to_user == user_id) | 
+                (Announcements.to_user == role) | 
+                (Announcements.to_user == "All")
+            ).all()
+            if not announcements:
+                raise HTTPException(status_code=404, detail="No announcements found")
+            return announcements
+    except HTTPException:
+        raise  
+    except Exception as e:
+        print(f"Error fetching announcements: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error")
