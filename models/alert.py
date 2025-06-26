f11a1445225def05b23d03807c1119517ce902fb
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-26 09:02:34 +0530

    Create annoucement model

---
+++ models/alert.py
@@ -0,0 +1,18 @@
+from sqlalchemy import (
+    Column, Integer, String, Boolean, DateTime,
+    ForeignKey, Enum, Date 
+)
+from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
+import enum
+from core.db import Base
+from zoneinfo import ZoneInfo
+
+class Announcements(Base):
+    __tablename__ = 'announcements'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(ZoneInfo("Asia/Kolkata")))
+    content = Column(String, nullable=False)
+    from_user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    to_user = Column(String, nullable=False)
