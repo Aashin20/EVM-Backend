fb742faa70fb5219f406ac8b496ec4fc0e20f8bc
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-26 09:02:08 +0530

    Create endpoint to view announcement

--- routers/announce_route.py
+++ routers/announce_route.py
@@ -7,3 +7,7 @@ router = APIRouter()
 @router.post("/create")
 async def create_announce(content: str,to_user: str,from_user_id: dict = Depends(get_current_user)):
     return create_announcement(content,from_user_id['user_id'],to_user)
+
+@router.get("/view")
+async def get_announce(current_user: dict = Depends(get_current_user)):
+    return view_announcements(current_user['user_id'],current_user['role'])
