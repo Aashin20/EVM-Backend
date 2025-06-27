927be477e851a8dbcb362588798d96b4cee86bf9
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-27 19:40:58 +0530

    Update announcement to have title

--- routers/announce_route.py
+++ routers/announce_route.py
@@ -5,8 +5,8 @@ from utils.authtoken import get_current_user
 router = APIRouter()
 
 @router.post("/create")
-async def create_announce(content: str,to_user: str,from_user_id: dict = Depends(get_current_user)):
-    return create_announcement(content,from_user_id['user_id'],to_user)
+async def create_announce(title:str,content: str,to_user: str,tag:str,from_user_id: dict = Depends(get_current_user)):
+    return create_announcement(title,content,tag,from_user_id['user_id'],to_user)
 
 @router.get("/view")
 async def get_announce(current_user: dict = Depends(get_current_user)):
