40cd463985169d2f06ab246b6fc1ecdecea4e4cb
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-06-28 07:41:18 +0530

    Create endpoint to dwnd N35

---
+++ routers/pdf_route.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter,Depends
+from annexure.N_35 import Form_N35,EVMPair
+from annexure.N_36 import Form_N36
+from annexure.pairing_sticker import pairing_sticker,EVMData
+from utils.authtoken import get_current_user
+
+router = APIRouter()
+
+@router.post("/N35")
+async def get_N35(data: EVMPair,allotment_order_no: str,current_user: dict=Depends(get_current_user)):
+    try:
+        filename = f"Form_N35_{allotment_order_no}.pdf"
+        return Form_N35(data, allotment_order_no, filename)
+    except Exception as e:
+        return {"error": str(e), "message": "Failed to generate Form N-35"}
