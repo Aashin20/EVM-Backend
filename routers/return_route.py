e7f547813a3f78c3b9f2870b8cc3aa6043f5c165
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-07-05 21:38:55 +0530

    Minor fix

--- routers/return_route.py
+++ routers/return_route.py
@@ -2,14 +2,14 @@ from core.return_ import status_change,decommission_evms,DecommissionModel
 from utils.authtoken import get_current_user
 from fastapi import APIRouter, Depends
 from typing import List
-
+from core.return_ import return_pending,return_queue,return_to_ecil
 router = APIRouter()
 
 
-@router.get('/{local_body_id}/{status}')
+@router.get('/change/{local_body_id}/{status}') #Used to change status of EVMs(Polling,Polled,Counted)
 async def to_polling(local_body_id:str,status:str,current_user: dict = Depends(get_current_user)):
     return status_change(local_body_id,status)
 
 @router.post('/decommission')
-async def evm_decommission(data: DecommissionModel):
+async def evm_decommission(data: DecommissionModel,current_user: dict = Depends(get_current_user)):
     return decommission_evms(data)
