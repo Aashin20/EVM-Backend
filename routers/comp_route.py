500683afbc36a76a6b617253a84da6f205a6ab0d
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-07-05 21:43:26 +0530

    Create endpoint to view MSR BU for a user

--- routers/comp_route.py
+++ routers/comp_route.py
@@ -98,3 +98,8 @@ async def get_msr_details_cu():
 @router.get("/msr/details/bu")
 async def get_msr_details_bu():
     return get_bu_data()
+
+@router.get("/msr/details/bu/user/")
+async def get_msr_details_bu_by_user(current_user: dict = Depends(get_current_user)):
+    return get_bu_data_by_user(current_user['user_id'])
+
