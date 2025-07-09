55272d1aa366ffc1bf1c3bc6c69cf2fa540cad43
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-07-10 03:31:28 +0530

    Create endpoint to gen box wise sticker

--- routers/pdf_route.py
+++ routers/pdf_route.py
@@ -3,6 +3,8 @@ from annexure.N_35 import Form_N35,EVMPair
 from annexure.N_36 import Form_N36
 from annexure.pairing_sticker import pairing_sticker,EVMData
 from utils.authtoken import get_current_user
+from core.flc import generate_box_wise_sticker
+from fastapi.responses import FileResponse
 
 router = APIRouter()
 
@@ -28,3 +30,7 @@ async def get_pairing_sticker(data_list: list[EVMData], current_user: dict = Dep
         return pairing_sticker(data_list, filename="pairing_sticker.pdf")
     except Exception as e:
         return {"error": str(e), "message": "Failed to generate pairing sticker"}
+    
+@router.get("/box-sticker/{district_id}")
+async def get_box_sticker(district_id:str,current_user: dict = Depends(get_current_user)):
+    return generate_box_wise_sticker(district_id)
