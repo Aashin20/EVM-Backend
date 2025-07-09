093a3e4ad605056ae14f6f2e7b860fac684ec5ab
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-07-10 01:24:48 +0530

    Minor fix

--- routers/master_route.py
+++ routers/master_route.py
@@ -1,18 +1,24 @@
 from fastapi import APIRouter   
 from core.user import (register, RegisterModel, view_users,UpdateUserModel,
                        edit_user,add_ps,approve_ps,reject_ps,view_ps,
-                       PollingStationModel,get_ps,mass_deactivate)
+                       PollingStationModel,get_ps,mass_deactivate,
+                       add_warehouse)
 from core.allotment import view_all_allotments_deo
 from utils.authtoken import get_current_user
 from fastapi import Depends
 from typing import List
 from core.components import get_details
+from pydantic import BaseModel
+
+class WarehouseCreate(BaseModel):
+    district_id: int
+    warehouse_name: str
 
 router = APIRouter()
 
 @router.post("/user/create")
 async def register_user(details: RegisterModel,current_user: dict = Depends(get_current_user)):
-    if current_user['role'] not in ['Developer', 'SEC']:
+    if current_user['role'] not in ['Developer', 'SEC', 'DEO']:    
         return {"status" : 401, "message": "Unauthorized access"}
     else:
         return register(details)
