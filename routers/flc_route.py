5678fd3e0d6665249c4dfa9e48a5ef00a6920b33
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-07-24 23:32:56 +0530

    Implement ratelimiting

--- routers/flc_route.py
+++ routers/flc_route.py
@@ -1,25 +1,29 @@
-from fastapi import APIRouter, Depends
-from core.flc import flc_cu, FLCCUModel, FLCBUModel,FLCDMMModel, flc_bu,flc_dmm
+from fastapi import APIRouter, Depends, Request  
+from core.flc import flc_cu, FLCCUModel, FLCBUModel, FLCDMMModel, flc_bu, flc_dmm
 from typing import List
 from utils.authtoken import get_current_user
-from fastapi import HTTPException,BackgroundTasks
+from fastapi import HTTPException, BackgroundTasks
+from utils.rate_limiter import limiter
 
 router = APIRouter()
 
 @router.post("/cu")
-async def flc_cu_bulk(data: List[FLCCUModel],background_tasks: BackgroundTasks, current_user: dict = Depends(get_current_user)):
-        return flc_cu(data,current_user['user_id'],background_tasks)
+@limiter.limit("30/minute")
+async def flc_cu_bulk(request: Request, data: List[FLCCUModel], background_tasks: BackgroundTasks, current_user: dict = Depends(get_current_user)): # Added Request parameter
+        return flc_cu(data, current_user['user_id'], background_tasks)
 
 @router.post("/bu")
-async def flc_bu_bulk(data: List[FLCBUModel],background_tasks: BackgroundTasks,current_user: dict = Depends(get_current_user)):
+@limiter.limit("30/minute")
+async def flc_bu_bulk(request: Request, data: List[FLCBUModel], background_tasks: BackgroundTasks, current_user: dict = Depends(get_current_user)): # Added Request parameter
     if current_user['role'] not in ['Developer', 'FLC Officer']:
         return {"status": 401, "message": "Unauthorized access"}
     else:
-        return flc_bu(data,current_user['user_id'],background_tasks)
+        return flc_bu(data, current_user['user_id'], background_tasks)
 
 @router.post("/dmm")
-async def flc_dmm_bulk(data: List[FLCDMMModel],background_tasks: BackgroundTasks,current_user: dict = Depends(get_current_user)):
+@limiter.limit("30/minute")
+async def flc_dmm_bulk(request: Request, data: List[FLCDMMModel], background_tasks: BackgroundTasks, current_user: dict = Depends(get_current_user)): # Added Request parameter
     if current_user['role'] not in ['Developer', 'FLC Officer']:
         return {"status": 401, "message": "Unauthorized access"}
     else:
-        return flc_dmm(data,current_user['user_id'],background_tasks)
+        return flc_dmm(data, current_user['user_id'], background_tasks)
