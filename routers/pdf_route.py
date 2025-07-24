7d8454dcf14a7b574df184b16f262a16034f19e9
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-07-24 19:36:02 +0530

    Implement jwt

--- routers/pdf_route.py
+++ routers/pdf_route.py
@@ -1,17 +1,29 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends,BackgroundTasks
 from annexure.N_35 import Form_N35,EVMPair
 from annexure.N_36 import Form_N36
 from annexure.pairing_sticker import pairing_sticker,EVMData
 from utils.authtoken import get_current_user
 from core.flc import generate_box_wise_sticker
 from fastapi.responses import FileResponse
+from core.appendix import generate_daily_flc_report,generate_flc_appendix2, generate_appendix3_for_district
+from pydantic import BaseModel
+from typing import List
+import uuid
 
 router = APIRouter()
+
+class Appendix3(BaseModel):
+    districtid: int
+    joining_date: str
+    members: List[str]
+    free_accommodation: bool
+    local_conveyance: bool 
+    relieving_date: str
 
 @router.post("/N35")
 async def get_N35(data: EVMPair,allotment_order_no: str,current_user: dict=Depends(get_current_user)):
     try:
-        filename = f"Form_N35_{allotment_order_no}.pdf"
+        filename = f"Form_N35_{allotment_order_no}_{uuid.uuid4().hex}.pdf"
         return Form_N35(data, allotment_order_no, filename)
     except Exception as e:
         return {"error": str(e), "message": "Failed to generate Form N-35"}
@@ -19,7 +31,7 @@ async def get_N35(data: EVMPair,allotment_order_no: str,current_user: dict=Depen
 @router.post("/N36")
 async def get_N36(data: EVMPair,allotment_order_no: str,current_user: dict=Depends(get_current_user)):
     try:
-        filename = f"Form_N36_{allotment_order_no}.pdf"
+        filename = f"Form_N36_{allotment_order_no}_{uuid.uuid4().hex}.pdf"
         return Form_N36(data, allotment_order_no, filename)
     except Exception as e:
         return {"error": str(e), "message": "Failed to generate Form N-36"}
@@ -27,7 +39,7 @@ async def get_N36(data: EVMPair,allotment_order_no: str,current_user: dict=Depen
 @router.post("/pairing_sticker")
 async def get_pairing_sticker(data_list: list[EVMData], current_user: dict = Depends(get_current_user)):
     try:
-        return pairing_sticker(data_list, filename="pairing_sticker.pdf")
+        return pairing_sticker(data_list, filename=f"pairing_sticker_{uuid.uuid4().hex}.pdf")
     except Exception as e:
         return {"error": str(e), "message": "Failed to generate pairing sticker"}
     
@@ -36,17 +48,17 @@ async def get_box_sticker(district_id:str,current_user: dict = Depends(get_curre
     return generate_box_wise_sticker(district_id)
 
 @router.get("/templates/add/{component_type}")
-async def get_add_template(component_type:str):
+async def get_add_template(component_type:str,current_user: dict=Depends(get_current_user)):
     return FileResponse(path=f"templates/Add_{component_type}.xlsx",media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',filename=f"Add_{component_type}")
 
 @router.get("/templates/flc/{component_type}")
-async def get_flc_template(component_type:str):
+async def get_flc_template(component_type:str,current_user: dict=Depends(get_current_user)):
     return FileResponse(path=f"templates/FLC_{component_type}.xlsx",media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',filename=f"FLC_{component_type}")
 
 @router.get("/annexure-2")
-async def get_attendance_reg():
+async def get_attendance_reg(current_user: dict=Depends(get_current_user)):
     return FileResponse(path="templates/Attendance.pdf",media_type="application/pdf",filename="Annexure-II")
 
 @router.get("/annexure-4")
-async def get_attendance_reg():
+async def get_attendance_reg(current_user: dict=Depends(get_current_user)):
     return FileResponse(path="templates/Physical_Verification.pdf",media_type="application/pdf",filename="Annexure-IV")
