3fcb8997c0507bcdf0319847563fa2f008681029
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-07-24 23:31:10 +0530

    Implement ratelimiting

--- routers/announce_route.py
+++ routers/announce_route.py
@@ -1,13 +1,26 @@
-from core.announcements import create_announcement,view_announcements
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter, Depends, Request
 from utils.authtoken import get_current_user
+from utils.rate_limiter import limiter
+from core.announcements import create_announcement, view_announcements
 
 router = APIRouter()
 
 @router.post("/create")
-async def create_announce(title:str,content: str,to_user: str,tag:str,from_user_id: dict = Depends(get_current_user)):
-    return create_announcement(title,content,tag,from_user_id['user_id'],to_user)
+@limiter.limit("30/minute")
+async def create_announce(
+    request: Request,  
+    title: str,
+    content: str,
+    to_user: str,
+    tag: str,
+    from_user_id: dict = Depends(get_current_user)
+):
+    return create_announcement(title, content, tag, from_user_id['user_id'], to_user)
 
 @router.get("/view")
-async def get_announce(current_user: dict = Depends(get_current_user)):
-    return view_announcements(current_user['user_id'],current_user['role'])
+@limiter.limit("30/minute")
+async def get_announce(
+    request: Request,  
+    current_user: dict = Depends(get_current_user)
+):
+    return view_announcements(current_user['user_id'], current_user['role'])
