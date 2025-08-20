7cf78793bc890017bf138250f91286839e064ed4
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-08-21 02:40:21 +0530

    Implement caching

--- routers/announce_route.py
+++ routers/announce_route.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends, Request
 from utils.authtoken import get_current_user
 from utils.rate_limiter import limiter
 from core.announcements import create_announcement, view_announcements
+from utils.cache_decorator import cache_response
+from utils.redis import RedisClient
 
 router = APIRouter()
 
@@ -15,9 +17,11 @@ async def create_announce(
     tag: str,
     from_user_id: dict = Depends(get_current_user)
 ):
+    await RedisClient.delete_pattern("announce*")
     return create_announcement(title, content, tag, from_user_id['user_id'], to_user)
 
 @router.get("/view")
+@cache_response(expire=3600, key_prefix="announce_view", include_user=True)
 @limiter.limit("30/minute")
 async def get_announce(
     request: Request,  
