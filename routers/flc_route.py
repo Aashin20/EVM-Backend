498f722b6a28fb4bcfae0feb76964c51222d45dc
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-08-21 02:40:30 +0530

    Implement caching

--- routers/flc_route.py
+++ routers/flc_route.py
@@ -4,13 +4,19 @@ from typing import List
 from utils.authtoken import get_current_user
 from fastapi import HTTPException, BackgroundTasks
 from utils.rate_limiter import limiter
+from utils.redis import RedisClient
+from utils.cache_decorator import cache_response
 
 router = APIRouter()
 
 @router.post("/cu")
 @limiter.limit("30/minute")
-async def flc_cu_bulk(request: Request, data: List[FLCCUModel], background_tasks: BackgroundTasks, current_user: dict = Depends(get_current_user)): # Added Request parameter
-        return flc_cu(data, current_user['user_id'], background_tasks)
+async def flc_cu_bulk(request: Request, data: List[FLCCUModel], background_tasks: BackgroundTasks, current_user: dict = Depends(get_current_user)):
+        if current_user['role'] not in ['Developer', 'FLC Officer']:
+            return {"status": 401, "message": "Unauthorized access"}
+        else:     
+            await RedisClient.delete_pattern("comp*") 
+            return flc_cu(data, current_user['user_id'], background_tasks)
 
 @router.post("/bu")
 @limiter.limit("30/minute")
@@ -18,6 +24,7 @@ async def flc_bu_bulk(request: Request, data: List[FLCBUModel], background_tasks
     if current_user['role'] not in ['Developer', 'FLC Officer']:
         return {"status": 401, "message": "Unauthorized access"}
     else:
+        await RedisClient.delete_pattern("comp*") 
         return flc_bu(data, current_user['user_id'], background_tasks)
 
 @router.post("/dmm")
@@ -26,14 +33,17 @@ async def flc_dmm_bulk(request: Request, data: List[FLCDMMModel], background_tas
     if current_user['role'] not in ['Developer', 'FLC Officer']:
         return {"status": 401, "message": "Unauthorized access"}
     else:
+        await RedisClient.delete_pattern("comp*") 
         return flc_dmm(data, current_user['user_id'], background_tasks)
     
 @router.get('/view/{component_type}/{district_id}')
+@cache_response(expire=3600, key_prefix="comp_flc_view", include_user=True)
 @limiter.limit("30/minute")
 async def view_flc_components_route(request: Request,component_type: str, district_id: str,current_user: dict = Depends(get_current_user)):
      return view_flc_components(component_type, district_id)
 
 @router.get('/summary')
+@cache_response(expire=3600, key_prefix="comp_flc_summary", include_user=True)
 @limiter.limit("30/minute")
 async def view_all_districts_flc_summary_route(request: Request,current_user: dict = Depends(get_current_user)):
     return view_all_districts_flc_summary()
